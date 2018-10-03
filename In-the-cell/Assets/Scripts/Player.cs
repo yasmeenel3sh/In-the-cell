823e3288ca93315be3abd0c295637addc5d9551f
yasmeenel3sh <yasmeen.abdelmohsen@student.guc.edu.eg>
2018-10-03 13:31:06 +0200

    scripts

--- In-the-cell/Assets/Scripts/Player.cs
+++ In-the-cell/Assets/Scripts/Player.cs
@@ -2,54 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
-	public BarScript bar;
-
+public class Player : MonoBehaviour
+{
     private float health;
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private AudioClip[] sounds;
+    private float counter;
+    private AudioSource audioSource;
+    // Use this for initialization
+    void Start()
+    {
         health = 100f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		UpdateHealthBar();
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 
-	void UpdateHealthBar() {
-		if (Input.GetKeyDown(KeyCode.Q)) {
-			bar.fillAmount -= 0.1;
-		}
-		if(Input.GetKeyDown(KeyCode.W)) {
-			bar.fillAmount += 0.1;
-		}
-	}
-
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter == 100)
+        {
+            health--;
+            counter = 0;
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided");
         if (other.tag == "cake")
         {
-
-        }else
-        if(other.tag == "butter")
+            if (health < 200)
+            {
+                health += 10f;
+                if (health > 200)
+                    health = 200;
+            }
+            this.audioSource.PlayOneShot(sounds[0]);
+        }
+        else
+        if (other.tag == "butter")
         {
+            this.audioSource.PlayOneShot(sounds[1]);
+            if (health < 200)
+            {
+                health += 10f;
+                if (health > 200)
+                    health = 200;
+            }
 
-        }else
+        }
+        else
         if (other.tag == "fruit")
         {
-
-        }else
+            if (health < 100)
+            {
+                health += 0.5f;
+                ;
+            }
+            this.audioSource.PlayOneShot(sounds[2]);
+        }
+        else
         if (other.tag == "meat")
         {
-
-        }else
-        if(other.tag == "insulin")
-        {
-            
+            if (health < 100)
+            {
+                health += 0.5f;
+            }
+            this.audioSource.PlayOneShot(sounds[3]);
         }
+        else
+        if (other.tag == "insulin")
+        {
+            if (health > 100)
+            {
+                health -= 2f;
+                if (health < 100)
+                    health = 100;
+            }
+            this.audioSource.PlayOneShot(sounds[4]);
+        }
+        else
+            if (other.tag == "lipase")
+        {
+            if (health > 100)
+            {
+                health -= 2f;
+                if (health < 100)
+                    health = 100;
+            }
+            this.audioSource.PlayOneShot(sounds[5]);
+        }
+        else if (other.tag == "slowDown")
+        {
+
+        }
+
+        Destroy(other.gameObject, 1.5f);
     }
-
-
 }
--- In-the-cell/Assets/Scripts/Spawns.cs
+++ In-the-cell/Assets/Scripts/Spawns.cs
@@ -2,40 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawns : MonoBehaviour {
+public class Spawns : MonoBehaviour
+{
     public GameObject[] spawns;
-    float speed;
-    float spawnTime ;
+   public float speed;
+    float spawnTime;
     float spawnTimer;
-    float speedTime ;
+    float speedTime;
     float speedTimer;
-	// Use this for initialization
-	void Start () {
-        spawnTime = 3;
+    float spawnCounter;
+    float speedCounter;
+    // Use this for initialization
+    void Start()
+    {
+        spawnTime = 5;
         spawnTimer = 0;
         speedTime = 1;
         speedTimer = 0;
-        speed = 100f;
-        
+        speed = 1f;
+        spawnCounter = 0;
+        speedCounter = 0;
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+
         spawnTimer += Time.deltaTime;
         if (spawnTimer >= spawnTime)
         {
             Spawn();
             spawnTimer = 0;
         }
+        spawnCounter++;
+        speedCounter++;
+        if (spawnCounter == 100)
+        {
+            if(spawnTime>0.7f)
+            spawnTime -= 0.1f;
+            spawnCounter = 0;
+        }
+        if (speedCounter == 100)
+        {
+            if (speed < 10)
+                speed += 0.2f;
+            speedCounter = 0;
+        }
 
-	}
+    }
     void Spawn()
     {
         int toBeSpawnedIndex = Random.Range(0, spawns.Length);
         float randomX = Random.Range(-2.5f, 2.5f);
         float step = speed * Time.deltaTime;
         Debug.Log(toBeSpawnedIndex);
-        GameObject toBeSpawned = Instantiate(spawns[toBeSpawnedIndex], new Vector3(randomX, transform.position.y, 10f), Quaternion.identity);
-   
+        GameObject toBeSpawned = Instantiate(spawns[toBeSpawnedIndex], new Vector3(randomX, 5f, 10f), Quaternion.identity);
+
     }
 }
